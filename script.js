144e9bd1421632622554559f010a2c91f94bf61c
Sean O <requiem.arrow@gmail.com>
2019-10-04 01:35:58 -0500

    Initial Commit

---
+++ script.js
@@ -0,0 +1,36 @@
+function nameInfo()  {
+    var firstName = document.getElementById("firstName").value;
+    var middleName = document.getElementById("middleName").value;
+    var lastName = document.getElementById("lastName").value;
+    var fullName = firstName + " " + middleName + " " + lastName;
+    document.getElementById("fullName").innerHTML = fullName;
+}
+console.log("If you are reading this, kudos.");
+console.log("More or less this is just a test for amusement.");
+console.log("Maybe a little learning too.");
+
+var anotherTest = "This is a test for data movement.";
+var triedTested = "This is also a test for data movement";
+var emptyVariable;
+var practice;
+
+var whisperHerName = "28";
+var whisperMyLove = "5";
+var whisperIsAWhisper = "11";
+var whisperIsLove = "36";
+
+if (whisperHerName <=29){
+    alert("I don't entirely get this.")    
+    console.log("This is interesting and odd, but funny.");
+}
+
+
+if (whisperMyLove == whisperIsAWhisper){
+    alert("Another thing to click, bahaha.");
+    console.log("Look at all the logging, I'm a lumberjack!");
+    console.log("...and I don't care!!");
+}
+else {
+    alert("Guess what this is?");
+}
+
